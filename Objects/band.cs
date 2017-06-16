a17a1879093b746dbaa3a66706881719b9e52ea7
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-16 09:36:42 -0700

    add constructor and private variables to Band Object Files

---
+++ Objects/band.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class Band
+  {
+    private int _id;
+    private string _name;
+
+    public Band(string name, int id = 0)
+    {
+      _id = id;
+      _name = name;
+    }
+  }
+}
