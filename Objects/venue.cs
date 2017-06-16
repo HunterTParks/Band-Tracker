883ebaa7e034b6b45c8a9d6415da8fb06ce44afc
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-16 08:44:15 -0700

    add GetAll method to venue objects file

--- Objects/venue.cs
+++ Objects/venue.cs
@@ -39,5 +39,34 @@ namespace BandTracker
         return (nameEquality && idEquality);
       }
     }
+
+    public static List<Venue> GetAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Venue> venues = new List<Venue>{};
+
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        Venue newVenue = new Venue(name, id);
+        venues.Add(newVenue);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
   }
 }
