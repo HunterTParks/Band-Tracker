157f246c3715eee8f7d8abbcfeb9ffec72266964
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-16 13:33:23 -0700

    add front end stuff and routing

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -21,6 +21,33 @@ namespace BandTracker
         List<Band> allBands = Band.GetAll();
         return View["bands.cshtml", allBands];
       };
+
+      Get["/bands/add"] = _ => {
+        return View["addBands.cshtml"];
+      };
+
+      Get["/venues/add"] = _ => {
+        return View["addVenues.cshtml"];
+      };
+
+      Post["/bands/add/success"] = _ => {
+        Band newBand = new Band(Request.Form["new-band"]);
+        newBand.Save();
+        return View["success.cshtml"];
+      };
+
+      Post["/venues/add/success"] = _ => {
+        Venue newVenue = new Venue(Request.Form["new-venue"]);
+        newVenue.Save();
+        return View["success.cshtml"];
+      };
+
+      Get["/bands/{id}"] = parameters => {
+        Band findBand = Band.Find(parameters.id);
+        return View["band.cshtml", findBand];
+      };
+
+      
     }
   }
 }
