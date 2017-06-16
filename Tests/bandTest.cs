da27bf49ecf0464848c8da965dada309daf3a15a
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-16 11:26:06 -0700

    add test method to bandTest 'AddVenueToBand'

--- Tests/bandTest.cs
+++ Tests/bandTest.cs
@@ -52,6 +52,27 @@ namespace BandTracker
       Assert.Equal(controlBand.GetName(), newBand.GetName());
     }
 
+    [Fact]
+    public void AddVenueToBand_Venue_Venue()
+    {
+      Venue newVenue = new Venue("Warped Tour");
+      newVenue.Save();
+
+      Venue newVenue2 = New Venue("Rose Quarter");
+      newVenue2.Save();
+
+      Band newBand1 = new Band("Issues");
+      newBand1.Save();
+
+      newBand1.AddVenue(newVenue());
+
+      List<Venue> testList = newBand1.GetVenues();
+      List<Venue> controlList = new List<Band>{newVenue, newVenue2};
+
+      Assert.Equal(controlList[0].GetName(), testList[0].GetName())
+
+    }
+
     public void Dispose()
     {
       Band.DeleteAll();
