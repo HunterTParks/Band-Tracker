0749333107773e29996a25466870cafced6f63f9
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-16 09:06:33 -0700

    add Save() method to Venues

--- Objects/venue.cs
+++ Objects/venue.cs
@@ -71,6 +71,33 @@ namespace BandTracker
       return venues;
     }
 
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO venues (name) OUTPUT INSERTED.id VALUES (@VenueName)", conn);
+
+      SqlParameter nameParameter = new SqlParameter("@VenueName", this.GetName());
+      cmd.Parameters.Add(nameParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
